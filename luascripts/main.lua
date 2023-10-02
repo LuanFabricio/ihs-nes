1d5149fa8268f65f12c14ba2deff31964f0e7217
LuanFabricio <luanlimaleite@hotmail.com>
2023-10-02 15:27:58 -0300

    adicionando instrução para resetar o jogo

--- luascripts/main.lua
+++ luascripts/main.lua
@@ -1,19 +1,16 @@
 require 'lib/mem_addr'
 
--- print(string.format("0x%x", memory.readbyte(MEMORY.board.player.x)))
--- memory.writebyte(MEMORY.board.player.x, bit.band(0x58))
--- print(string.format("0x%x", memory.readbyte(MEMORY.board.player.x)))
+local function draw_winner_message(winner)
+	local x = 100
+	if winner == "PLAYER" then
+		x = 88
+	end
+	gui.text(x, 100, winner .. " WINS!")
+end
 
-local test_input = {
-	right = true,
-
-}
-local inputs = {
-	{ right = true },
-	{ left = true },
-	{ up = true },
-	{ down = true },
-}
+local function draw_reset_message()
+	gui.text(48, 112, "Press Start to reset the game.")
+end
 
 require 'lib.board'
 require 'lib.input'
@@ -30,39 +27,35 @@ Board.pieces_len = Board:count_pieces(memory)
 Board:copy_in_game_board(memory)
 print(Board.board)
 
-local last_ai_move = ''
-local winner = ''
+local last_ai_move = ""
 
 while(true) do
 	Board.pieces_len = Board:count_pieces(memory)
 	Board:copy_in_game_board(memory)
 
-	if not Board:check_king(false) then
-		print("PLAYER WINS!")
-		winner = "PLAYER WINS!"
-		break
-	else
-		if not Board:check_king(true) then
-			print()
-			winner = "AI WINS!"
-			break
-		end
+	if joypad.read(1).start then
+		Board:init_board(memory)
+		last_ai_move = ""
 	end
 
-	local zapper_info = zapper.read()
+	if not Board:check_king(false) then
+		print("PLAYER WINS!")
+		draw_winner_message("PLAYER")
+		draw_reset_message()
+	elseif not Board:check_king(true) then
+		print("AI WINS!")
+		draw_winner_message("AI")
+		draw_reset_message()
+	else
+		local zapper_info = zapper.read()
 
-	if not Board.is_player_turn then
-		last_ai_move = Board:AI_move(memory)
-	else if zapper_info.fire == 1 then
+		if not Board.is_player_turn then
+			last_ai_move = Board:AI_move(memory)
+		elseif zapper_info.fire == 1 then
 			current_piece = Input:handle_player_click(zapper_info, memory, Board, current_piece)
 		end
 	end
 
 	gui.text(16, 16, "AI move: " .. last_ai_move)
-	emu.frameadvance()
-end
-
-while true do
-	gui.text(104, 100, winner)
 	emu.frameadvance()
 end
